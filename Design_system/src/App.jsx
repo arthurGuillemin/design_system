6d91dd1e17918d4e1cc0ecbc34498282d2eb0f89
Steve <steve@MacBook-Pro-de-Steve.local>
2025-01-07 12:07:01 +0100

    ajout du Header

---
+++ Design_system/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Votre contenu principal ici */}
+        <h1>Welcome to Bloom</h1>
+      </main>
+    </div>
+  );
+};
+
+export default App;
