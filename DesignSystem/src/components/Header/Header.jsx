3c259858fd753cb1e7cfafc74a0bd2b4f03a2bd8
arthurGuillemin <aguillemin291@gmail.com>
2025-01-08 16:29:15 +0100

    Fix encore

--- DesignSystem/src/components/Header/Header.jsx
+++ DesignSystem/src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { ReactComponent as Logo } from '../../assets/icons/Logo.svg'; // Import SVG as React Component
-import { ReactComponent as AccountIcon } from '../../assets/icons/Account=Account.svg';
+import IconButton from '../IconBtn/IconButton';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
-        <Logo className={styles.logo} />
+        <img src='.//assets/icons/Logo.svg'></img>
         <h1 className={styles.title}>BLOOM</h1>
       </div>
       <div className={styles.accountContainer}>
-        <AccountIcon className={styles.accountIcon} />
+        <IconButton type={"account"}/>
       </div>
     </header>
   );
