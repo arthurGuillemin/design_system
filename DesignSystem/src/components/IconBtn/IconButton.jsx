eab696b39191d66382b87dbfa2e5fb2258542779
Emy <emilie.caverne@outlook.fr>
2025-01-08 13:27:59 +0100

    Composant Icon

--- DesignSystem/src/components/IconBtn/IconButton.jsx
+++ DesignSystem/src/components/IconBtn/IconButton.jsx
@@ -2,8 +2,23 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import './IconButton.module.css';
 
-const IconButton = ({ defaultIcon, pressedIcon, alt, onClick }) => {
+import DefaultAccountIcon from '../../assets/icons/Account=Default.svg';
+import PressedAccountIcon from '../../assets/icons/Account=Account.svg';
+import DefaultArrowIcon from '../../assets/icons/arrow_forward_ios.svg';
+import PressedArrowIcon from '../../assets/icons/arrow_forward_ios2.svg';
+import DefaultPasswordIcon from '../../assets/icons/Password-Visibility=Default.svg';
+import PressedPasswordIcon from '../../assets/icons/Password-Visibility=Clicked.svg';
+
+
+const IconButton = ({ type, alt, onClick, isActive }) => {
   const [isPressed, setIsPressed] = useState(false);
+
+  const iconSets = {
+    account: { default: DefaultAccountIcon, pressed: PressedAccountIcon },
+    arrow: { default: PressedArrowIcon, pressed: DefaultArrowIcon },
+    password: { default: DefaultPasswordIcon, pressed: PressedPasswordIcon },
+  };
+  const icons = iconSets[type] || iconSets['account'];
 
   const handleMouseDown = () => setIsPressed(true);
   const handleMouseUp = () => setIsPressed(false);
@@ -16,7 +31,7 @@ const IconButton = ({ defaultIcon, pressedIcon, alt, onClick }) => {
       onClick={onClick}
     >
       <img
-        src={isPressed ? pressedIcon : defaultIcon}
+        src={isActive || isPressed ? icons.pressed : icons.default}
         alt={alt}
         className="icon-image"
       />
@@ -25,15 +40,17 @@ const IconButton = ({ defaultIcon, pressedIcon, alt, onClick }) => {
 };
 
 IconButton.propTypes = {
-  defaultIcon: PropTypes.string.isRequired,
-  pressedIcon: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['account', 'arrow', 'password']),
   alt: PropTypes.string,
   onClick: PropTypes.func,
+  isActive: PropTypes.bool,
 };
 
 IconButton.defaultProps = {
+  type: 'account',
   alt: 'Icon Button',
   onClick: () => {},
+  isActive: false,
 };
 
 export default IconButton;
