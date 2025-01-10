102d5a06a05997526bc5322aae19edb764299889
Emy <emilie.caverne@outlook.fr>
2025-01-10 13:45:50 +0100

    link header

--- DesignSystem/src/components/Header/Header.jsx
+++ DesignSystem/src/components/Header/Header.jsx
@@ -2,17 +2,18 @@ import styles from './Header.module.css';
 import IconButton from '../IconBtn/IconButton';
 import Logo from '../../assets/img/logo.svg';
 import Dropdown from '../Dropdown/Dropdown';
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
-        <img src={Logo}></img>
+        <Link to="/"><img src={Logo} href={"/"}></img></Link>
         <h1 className={styles.title}>BLOOM</h1>
       </div>
       <div className={styles.accountContainer}>
-        <IconButton type={"account"} className={styles.account} /**link={"/profil"}*//>
+        <Link to="/login"><IconButton type={"account"} className={styles.account}/></Link>
         <Dropdown className={styles.Dropdown}/>
       </div>
     </header>
